700e8578be315ef0c10bd296648c03eb896ac847
nlgrqve <tonkabits@gmail.com>
2021-08-07 21:04:41 -0400

    initial commit

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials('views/partials');
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+    // const gotBeers = punkAPI.getBeers().then(() => {
+    // console.log(gotBeers)
+    // res.render('beers', {gotBeers});
+
+    const gotBeers = punkAPI.getBeers()
+    .then(beersFromApi =>  {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beersFromApi })
+
+    })
+    .catch(error => console.log(error));
+    
+  });
+
+app.get('/random-beer', (req, res) => {
+    const gotRandom = punkAPI.getRandom()
+    // .then( beer => res.render('random-beer', {beer2: beer[0]} ))
+    .then( beer => res.render('random-beer', {beer} ))
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
